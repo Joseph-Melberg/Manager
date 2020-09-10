fe2865179aa4caffedfc5799d26127dbd381a0df
Joseph Melberg <vtechdelete@gmail.com>
2020-09-09 20:00:46 -0500

    initial

---
+++ OnlineOfflineReaderService/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace OnlineOfflineReaderService
+{
+    class Program
+    {
+        static string QueueName = "Reader";
+        static async Task Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "life";
+            factory.Password = "conway";
+            factory.VirtualHost = "/";
+            factory.DispatchConsumersAsync = true;
+            factory.HostName = "10.0.1.3";
+            factory.ClientProvidedName = "app:audit component:event-consumer";
+            IConnection connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("inter", ExchangeType.Fanout,true);
+            channel.QueueDeclare(QueueName, false, false, false, null);
+            channel.QueueBind(QueueName, "inter", "/life", null);
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            
+            consumer.Received += async (ch, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                // copy or deserialise the payload
+                // and process the message
+                // ...
+
+                channel.BasicAck(ea.DeliveryTag, false);
+                await Task.Yield();
+
+            };
+            String consumerTag = channel.BasicConsume(QueueName, false, consumer);
+            while (true)
+            {
+                await Task.Yield();
+            }
+        }
+    }
+}
