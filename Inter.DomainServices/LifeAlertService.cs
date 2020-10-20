32803a8bb87928b35d42a11f4fd3c4fe23a9ba51
Joseph Melberg <vtechdelete@gmail.com>
2020-10-20 07:49:47 -0500

    My logic was wrong, also added a little debug printing

--- Inter.DomainServices/LifeAlertService.cs
+++ Inter.DomainServices/LifeAlertService.cs
@@ -15,35 +15,46 @@ namespace Inter.DomainServices
 
         public async void Do()
         {
+
             //Gather the entries that need to be announced
             var stati = _infra.GetStatuses();
             foreach( var nodeState in stati)
             {
-                var needsAnnouncing = !nodeState.announced;
-                var isStale = nodeState.timestamp.AddMinutes(5) < DateTime.Now;
-                var isAlive = nodeState.online;
-
-                Console.WriteLine($"{nodeState.name} is \n not announced:{needsAnnouncing} \n stale:{isStale} \n alive {isAlive}");
-                if (isAlive)
+                try
                 {
 
-                    if (isStale)
+
+                    var announcedState = nodeState.announced;
+                    var isStale = nodeState.timestamp.AddMinutes(5) < DateTime.Now;
+                    var isAlive = nodeState.online;
+
+                    Console.WriteLine($"{nodeState.name} is \n announce alive:{announcedState} \n stale:{isStale} \n alive {isAlive}");
+                    if (isAlive)
                     {
-                        Console.WriteLine($"{nodeState.name} is dead");
-                        _infra.SendMessage("6302478698@txt.att.net", "Report", $"{nodeState.name} is offline");
-                        nodeState.announced = true;
-                        nodeState.online = false;
-                        await _infra.UpdateNode(nodeState);
-                    }
-                    else if (needsAnnouncing)
-                    {
-                        Console.WriteLine($"{nodeState.name} is alive");
-                        _infra.SendMessage("6302478698@txt.att.net", "Report", $"{nodeState.name} is online");
-                        nodeState.announced = true;
-                        await _infra.UpdateNode(nodeState);
+
+                        if (isStale)
+                        {
+                            Console.WriteLine($"{nodeState.name} is dead");
+                            _infra.SendMessage("6302478698@txt.att.net", "Report", $"{nodeState.name} is offline");
+                            nodeState.announced = false;
+                            nodeState.online = false;
+                            await _infra.UpdateNode(nodeState);
+                        }
+                        else if (!announcedState)
+                        {
+                            Console.WriteLine($"{nodeState.name} is alive");
+                            _infra.SendMessage("6302478698@txt.att.net", "Report", $"{nodeState.name} is online");
+                            nodeState.announced = true;
+                            await _infra.UpdateNode(nodeState);
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
+            
             Console.Write("done");
         }
     }
