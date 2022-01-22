4597927332866280b1b8b635a91b80e5c1d29e9c
Joseph Melberg <joseph.melberg@myqops.com>
2022-01-21 20:27:54 -0600

    Forgot to update the deploy json

--- Applications/Inter.LifeAlertAppService/Register.cs
+++ Applications/Inter.LifeAlertAppService/Register.cs
@@ -1,13 +1,5 @@
 ﻿using System;
 using Inter.Dependency;
-using Inter.DomainServices;
-using Inter.DomainServices.Core;
-using Inter.Infrastructure.Core;
-using Inter.Infrastructure.Corral;
-using Inter.Infrastructure.MySQL.Contexts;
-using Inter.Infrastructure.MySQL.Repositories;
-using Inter.Infrastructure.Services;
-using Melberg.Infrastructure.MySql;
 using Microsoft.Extensions.DependencyInjection;
 namespace Inter.LifeAlertAppService;
 public class Register
