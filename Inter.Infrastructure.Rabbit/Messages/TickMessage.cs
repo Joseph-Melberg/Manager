0246e926161952bcf0bc6d20463797b3bf1eb859
Joseph-Melberg <dasbasdo@gmail.com>
2022-03-06 16:56:34 -0600

    Update TickMessage.cs

--- Inter.Infrastructure.Rabbit/Messages/TickMessage.cs
+++ Inter.Infrastructure.Rabbit/Messages/TickMessage.cs
@@ -4,7 +4,11 @@ namespace Inter.Infrastructure.Rabbit.Messages;
 
 public class TickMessage : StandardMessage
 {
-    public TickMessage() { }
+    public TickMessage() 
+    {
+        Timestamp = DateTime.UtcNow;
+    }
 
+    public DateTime Timestamp {get; private set;}
     public override string GetRoutingKey() => "tick.second";
 }
