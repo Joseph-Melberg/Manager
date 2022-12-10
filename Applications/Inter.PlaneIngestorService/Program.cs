c7f7441e545d3f34e357c83eee0834bc5c6dab37
Joseph-Melberg <jbmelberg@eiu.edu>
2022-12-10 14:49:42 -0600

    Update Program.cs

--- Applications/Inter.PlaneIngestorService/Program.cs
+++ Applications/Inter.PlaneIngestorService/Program.cs
@@ -4,5 +4,10 @@ namespace Inter.PlaneIngestorService;
 
 class Program
 {
-    static async Task Main(string[] args) => await MelbergHost.CreateDefaultApp<Startup>().Build().StartAsync();
+    static async Task Main()
+    {
+        Console.Write("A");
+        await MelbergHost.CreateDefaultApp<Startup>().Build().StartAsync();
+
+    }
 }
