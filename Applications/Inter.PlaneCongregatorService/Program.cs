e93654424fb387484bf27378da2864cd8eb8ea6e
Joseph-Melberg <joseph.melberg@myqops.com>
2023-06-20 16:24:23 -0500

    Whoops

--- Applications/Inter.PlaneCongregatorService/Program.cs
+++ Applications/Inter.PlaneCongregatorService/Program.cs
@@ -8,36 +8,10 @@ class Program
 {
     static async Task Main(string[] args)  
     {
-        var j = MelbergHost
+        await MelbergHost
             .CreateDefaultApp<Startup>()
-            .Build();
-           var p =  j.Services.GetService<IMetricPublisher>();
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           await Task.Delay(10);
-           p.SendMetric(4000,DateTime.UtcNow);
-           p.SendMetric(4000,DateTime.UtcNow);
-           p.SendMetric(4000,DateTime.UtcNow);
-           p.SendMetric(4000,DateTime.UtcNow);
-           p.SendMetric(4000,DateTime.UtcNow);
-           p.SendMetric(4000,DateTime.UtcNow);
-           p.SendMetric(4000,DateTime.UtcNow);
-            //.Begin(CancellationToken.None);
+            .Build()
+            .Begin(CancellationToken.None);
 
         //await j;
 }
